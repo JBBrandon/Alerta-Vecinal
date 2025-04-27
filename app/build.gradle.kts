d914153922646d18207ce61b8f34af3172bf9d26
Danluis <romanimoradanluis@gmail.com>
2025-04-26 22:16:56 -0500

    logic corregido

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    //id("com.google.gms.google-services")
 }
 
 android {
@@ -40,4 +41,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.code.gson:gson:2.8.9")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    //implementation ("com.google.firebase:firebase-bom:32.3.1")
+    //implementation ("com.google.firebase:firebase-auth-ktx")
+   //implementation ("com.google.firebase:firebase-auth")
+
 }
